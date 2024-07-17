443383213481f1813ac2171e2d8d5cf3d97a2256
DataScientist2807 <52509318+DataScientist2807@users.noreply.github.com>
2024-07-17 22:12:37 +0200

    Login & Register Request in Postman

--- larac_api_master/app/Http/Controllers/AuthController.php
+++ larac_api_master/app/Http/Controllers/AuthController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\ApiLoginRequest;
 use App\Traits\ApiResponses;
 use Illuminate\Http\Request;
 
@@ -9,10 +10,15 @@ class AuthController extends Controller
 {
     use ApiResponses;
 
-    public function login() {
-        return $this->ok('Hello 2, Login from Trait');
+    public function login(ApiLoginRequest $request) {
+        return $this->ok($request->get('email'));
+        /* return $this->ok('Hello 2, Login from Trait'); */
         /* return response()->json([
             'message' => 'Hello, login from Controller'
         ], 200); */
+    }
+
+    public function register() {
+        return $this->ok('register');
     }
 }
