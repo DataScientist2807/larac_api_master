6752483fdd2ee6490c43c92e63e4fb56bc9a139b
DataScientist2807 <52509318+DataScientist2807@users.noreply.github.com>
2024-07-18 11:00:56 +0200

    Token authentication

--- larac_api_master/routes/api_v1.php
+++ larac_api_master/routes/api_v1.php
@@ -1,7 +1,6 @@
 <?php
 
 use App\Http\Controllers\API\V1\TicketController;
-use App\Http\Controllers\AuthController;
 use App\Models\Ticket;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -28,7 +27,7 @@ use Illuminate\Support\Facades\Route;
     ], 200);
 }); */
 
-Route::apiResource('tickets', TicketController::class);
+Route::middleware('auth:sanctum')->apiResource('tickets', TicketController::class);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
