719bc92a94c0e4408e9831c376e219556fd170cd
DataScientist2807 <52509318+DataScientist2807@users.noreply.github.com>
2024-07-18 06:47:42 +0200

    API versioning

--- larac_api_master/routes/api_v1.php
+++ larac_api_master/routes/api_v1.php
@@ -1,9 +1,11 @@
 <?php
 
+use App\Http\Controllers\API\V1\TicketController;
 use App\Http\Controllers\AuthController;
 use App\Models\Ticket;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+
 
 
 
@@ -26,13 +28,7 @@ use Illuminate\Support\Facades\Route;
     ], 200);
 }); */
 
-Route::post('/login', [AuthController::class, 'login']);
-Route::post('/register', [AuthController::class, 'register']);
-/* Route::get('/tickets', function() {
-    return Ticket::all();
-}); */
-
-
+Route::apiResource('tickets', TicketController::class);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
