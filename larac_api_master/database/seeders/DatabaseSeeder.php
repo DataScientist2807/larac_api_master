b8271e4a14d4cf415695028de3ebb5248fac540f
DataScientist2807 <52509318+DataScientist2807@users.noreply.github.com>
2024-07-18 06:24:49 +0200

    Setup tickets API

--- larac_api_master/database/seeders/DatabaseSeeder.php
+++ larac_api_master/database/seeders/DatabaseSeeder.php
@@ -12,6 +12,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        $users = \App\Models\User::factory(10)->create();
+
+        \App\Models\Ticket::factory(100)
+            ->recycle($users)
+            ->create();
         // \App\Models\User::factory(10)->create();
 
         // \App\Models\User::factory()->create([
--- larac_api_master/routes/api.php
+++ larac_api_master/routes/api.php
@@ -1,8 +1,10 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Models\Ticket;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+
 
 
 /*
@@ -16,6 +18,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// http://localhost:8000/api/users/{id}/delete
+
 /* Route::get('/', function() {
     return response()->json([
         'message' => 'Hello, API'
@@ -24,6 +28,9 @@ use Illuminate\Support\Facades\Route;
 
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/register', [AuthController::class, 'register']);
+Route::get('/tickets', function() {
+    return Ticket::all();
+});
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
