ccee65ccfeba5df3040b7d91f9cbaf0c97b1b5dd
DataScientist2807 <52509318+DataScientist2807@users.noreply.github.com>
2024-07-19 06:23:00 +0200

    Writing Filters

--- larac_api_master/app/Http/Controllers/API/V1/TicketController.php
+++ larac_api_master/app/Http/Controllers/API/V1/TicketController.php
@@ -2,23 +2,20 @@
 
 namespace App\Http\Controllers\API\V1;
 
-use App\Http\Controllers\Api\V1\ApiController;
 use App\Http\Requests\API\V1\StoreTicketRequest;
 use App\Http\Requests\API\V1\UpdateTicketRequest;
 use App\Http\Resources\V1\TicketResource;
 use App\Models\Ticket;
+use App\Http\Filters\V1\TicketFilter;
 
 class TicketController extends ApiController
 {
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(TicketFilter $filters)
     {
-        if ($this->include('author')) {
-            return TicketResource::collection(Ticket::with('user')->paginate());
-        }
-        return TicketResource::collection(Ticket::paginate());
+        return TicketResource::collection(Ticket::filter($filters)->paginate());
     }
 
       /**
@@ -39,9 +36,6 @@ class TicketController extends ApiController
         }
         return new TicketResource($ticket);
     }
-
-
-
     /**
      * Update the specified resource in storage.
      */
--- larac_api_master/app/Models/Ticket.php
+++ larac_api_master/app/Models/Ticket.php
@@ -5,13 +5,19 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use App\Http\Filters\V1\QueryFilter;
+use Illuminate\Database\Eloquent\Builder;
 
 class Ticket extends Model
 {
     use HasFactory;
 
-    public function user(): BelongsTo {
-        return $this->belongsTo(User::class);
+    public function author(): BelongsTo {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+
+    public function scopeFilter(Builder $builder, QueryFilter $filters) {
+        return $filters->apply($builder);
     }
 
 }
